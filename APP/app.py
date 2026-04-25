1fe76717bcaf1410f198f0bf050cfda2b0fea62f
Vitor Hugo Marques Lorenco <hmlvitor@gmail.com>
2026-04-25 12:01:01 -0300

    Criação do app.py e configuração da página

---
+++ APP/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+from pathlib import Path
+
+# Configuração da página
+st.set_page_config(
+    page_title="Projeto Integrador - Grupo 28",
+    page_icon="🚀",  # O emoji pode ser alterado caso o grupo queira
+    layout="wide"
+)
